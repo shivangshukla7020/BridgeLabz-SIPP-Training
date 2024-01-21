61938d993a299e9677a3dea39b9b0ae2d7a4d18c
shivangshukla7020 <shivangshukla201@gmail.com>
2024-01-22 00:00:00 +0530

    [shivang] Add : JAVA-CONSTRUCTORS-VARIABLES-MODIFIERS/JAVA-VARIABLES/Product.java

---
+++ JAVA-CONSTRUCTORS-VARIABLES-MODIFIERS/JAVA-VARIABLES/Product.java
@@ -0,0 +1,20 @@
+class Product {
+    String productName;
+    double price;
+    static int totalProducts = 0;
+
+    Product(String productName, double price) {
+        this.productName = productName;
+        this.price = price;
+        totalProducts++;
+    }
+
+    void displayProductDetails() {
+        System.out.println("Product Name: " + productName);
+        System.out.println("Price: Rs. " + price);
+    }
+
+    static void displayTotalProducts() {
+        System.out.println("Total Products: " + totalProducts);
+    }
+}
