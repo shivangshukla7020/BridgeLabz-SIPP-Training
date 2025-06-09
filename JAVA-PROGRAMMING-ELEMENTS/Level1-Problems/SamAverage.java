dcf038064169708c895b2e933f36512e3dffa844
shivangshukla7020 <shivangshukla201@gmail.com>
2025-06-10 00:00:00 +0530

    [shivang] Add : E:\College\Emersion\JAVA-PROGRAMMING-ELEMENTS\Level1-Problems\SamAverage.java

---
+++ JAVA-PROGRAMMING-ELEMENTS/Level1-Problems/SamAverage.java
@@ -0,0 +1,9 @@
+public class SamAverage{
+    public static void main(String[] args){
+        int maths = 94;
+        int chemistry = 95;
+        int physics = 96;
+        int average = (maths + chemistry + physics)/3;
+        System.out.println("Sam's average marks in PCM is " + average);
+    }
+}
