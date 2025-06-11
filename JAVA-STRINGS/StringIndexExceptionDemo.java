7e4bb5c30feed541ef674265bfd000aa0c796b6d
shivangshukla7020 <shivangshukla201@gmail.com>
2025-06-12 00:00:00 +0530

    [shivang] Add : E:\College\Emersion\JAVA-STRINGS\StringIndexExceptionDemo.java

---
+++ JAVA-STRINGS/StringIndexExceptionDemo.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class StringIndexExceptionDemo {
+    public static void generateException(String text) {
+        System.out.println(text.charAt(text.length())); 
+    }
+
+    public static void handleException(String text) {
+        try {
+            System.out.println(text.charAt(text.length()));
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("Caught StringIndexOutOfBoundsException");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.next();
+        try {
+            generateException(text);
+        } catch (Exception e) {
+            handleException(text);
+        }
+    }
+}
