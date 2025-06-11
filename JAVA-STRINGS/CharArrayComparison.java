6e64d88cfd89374cb5f85576d68861c941c8ebe8
shivangshukla7020 <shivangshukla201@gmail.com>
2025-06-12 00:00:00 +0530

    [shivang] Add : E:\College\Emersion\JAVA-STRINGS\CharArrayComparison.java

---
+++ JAVA-STRINGS/CharArrayComparison.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class CharArrayComparison {
+    public static char[] getChars(String text) {
+        char[] chars = new char[text.length()];
+        for (int i = 0; i < text.length(); i++) {
+            chars[i] = text.charAt(i);
+        }
+        return chars;
+    }
+
+    public static boolean compareArrays(char[] a, char[] b) {
+        if (a.length != b.length) return false;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.next();
+        char[] userArray = getChars(text);
+        char[] builtInArray = text.toCharArray();
+        System.out.println("Are arrays equal? " + compareArrays(userArray, builtInArray));
+    }
+}
