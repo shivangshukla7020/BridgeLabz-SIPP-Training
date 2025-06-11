5f7a81ab5b35c919c11023e24bc6caba6f10e692
shivangshukla7020 <shivangshukla201@gmail.com>
2025-06-12 00:00:00 +0530

    [shivang] Add : E:\College\Emersion\JAVA-STRINGS\NumberFormatDemo.java

---
+++ JAVA-STRINGS/NumberFormatDemo.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class NumberFormatDemo {
+    public static void generateException(String text) {
+        int num = Integer.parseInt(text);
+        System.out.println(num);
+    }
+
+    public static void handleException(String text) {
+        try {
+            int num = Integer.parseInt(text);
+            System.out.println(num);
+        } catch (NumberFormatException e) {
+            System.out.println("Caught NumberFormatException");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.next();
+        try {
+            generateException(text);
+        } catch (Exception e) {
+            handleException(text);
+        }
+    }
+}
