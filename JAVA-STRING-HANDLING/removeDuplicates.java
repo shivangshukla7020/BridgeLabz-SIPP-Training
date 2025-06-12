0b3eae6ab8efd720c7cce76ffa2172f95a39f953
shivangshukla7020 <shivangshukla201@gmail.com>
2025-06-13 00:00:00 +0530

    [shivang] Add : E:\College\Emersion\JAVA-STRING-HANDLING\removeDuplicates.java

---
+++ JAVA-STRING-HANDLING/removeDuplicates.java
@@ -0,0 +1,12 @@
+
+public class removeDuplicates {
+    public static void main(String[] args) {
+        String str = "programming";
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (result.indexOf(ch) == -1) result += ch;
+        }
+        System.out.println("Modified String: " + result);
+    }
+}
