60e052acf856920c7129d349a6e2ac3e94aa6ced
shivangshukla7020 <shivangshukla201@gmail.com>
2025-07-10 09:29:44 +0530

    [Shivang] Add : RideHailingApplication.java

---
+++ ENCAPSULATION_POLYMORPHISM_INTERFACE_ABSTRACT_CLASS/RideHailingApplication.java
@@ -0,0 +1,8 @@
+// RideHailingApplication.java
+// Description: Develop a ride-hailing application with vehicle types and GPS interfaces using polymorphism.
+
+public class RideHailingApplication {
+    public static void main(String[] args) {
+        // Implementation goes here
+    }
+}
