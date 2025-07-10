28ee263c2b93192940c65f9f166c575ad7de516e
shivangshukla7020 <shivangshukla201@gmail.com>
2025-07-10 09:29:44 +0530

    [Shivang] Add : HospitalPatientManagement.java

---
+++ ENCAPSULATION_POLYMORPHISM_INTERFACE_ABSTRACT_CLASS/HospitalPatientManagement.java
@@ -0,0 +1,8 @@
+// HospitalPatientManagement.java
+// Description: Design a hospital patient management system using interfaces and encapsulation.
+
+public class HospitalPatientManagement {
+    public static void main(String[] args) {
+        // Implementation goes here
+    }
+}
