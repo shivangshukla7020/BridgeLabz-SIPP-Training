ada0a36e2d47fa7e1257c48c898d1452f34be5a0
shivangshukla7020 <shivangshukla201@gmail.com>
2025-07-10 09:29:43 +0530

    [Shivang] Add : BankingSystem.java

---
+++ ENCAPSULATION_POLYMORPHISM_INTERFACE_ABSTRACT_CLASS/BankingSystem.java
@@ -0,0 +1,8 @@
+// BankingSystem.java
+// Description: Create a banking system with various account types and loan eligibility using interfaces and polymorphism.
+
+public class BankingSystem {
+    public static void main(String[] args) {
+        // Implementation goes here
+    }
+}
