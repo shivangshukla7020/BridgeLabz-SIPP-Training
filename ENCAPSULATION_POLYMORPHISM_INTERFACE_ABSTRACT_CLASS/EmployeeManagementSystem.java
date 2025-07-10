da0529e0de7680c9b7fc4a2cdfd95ebd1a581de2
shivangshukla7020 <shivangshukla201@gmail.com>
2025-07-10 09:29:43 +0530

    [Shivang] Add : EmployeeManagementSystem.java

---
+++ ENCAPSULATION_POLYMORPHISM_INTERFACE_ABSTRACT_CLASS/EmployeeManagementSystem.java
@@ -0,0 +1,8 @@
+// EmployeeManagementSystem.java
+// Description: Build an employee management system with abstract classes, interfaces, encapsulation, and polymorphism.
+
+public class EmployeeManagementSystem {
+    public static void main(String[] args) {
+        // Implementation goes here
+    }
+}
