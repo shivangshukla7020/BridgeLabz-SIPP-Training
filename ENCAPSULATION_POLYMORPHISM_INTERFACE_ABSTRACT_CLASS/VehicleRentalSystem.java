e8ba7af67bf8b5164e755f83608b3ebf82403e1e
shivangshukla7020 <shivangshukla201@gmail.com>
2025-07-10 09:29:43 +0530

    [Shivang] Add : VehicleRentalSystem.java

---
+++ ENCAPSULATION_POLYMORPHISM_INTERFACE_ABSTRACT_CLASS/VehicleRentalSystem.java
@@ -0,0 +1,8 @@
+// VehicleRentalSystem.java
+// Description: Design a system to manage vehicle rentals with polymorphism and encapsulated insurance logic.
+
+public class VehicleRentalSystem {
+    public static void main(String[] args) {
+        // Implementation goes here
+    }
+}
