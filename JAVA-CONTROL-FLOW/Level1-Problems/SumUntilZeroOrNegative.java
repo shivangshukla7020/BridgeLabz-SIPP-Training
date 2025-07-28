1afcd894d192ce07e83da43af867c4caf7b7a00a
shivangshukla7020 <shivangshukla201@gmail.com>
2025-07-29 00:00:00 +0530

    [shivang] Add : JAVA-CONTROL-FLOW/Level1-Problems/SumUntilZeroOrNegative.java

---
+++ JAVA-CONTROL-FLOW/Level1-Problems/SumUntilZeroOrNegative.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class SumUntilZeroOrNegative {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double total = 0;
+
+        while (true) {
+            double input = sc.nextDouble();
+            if (input <= 0) break;
+            total += input;
+        }
+        System.out.println("Total sum is: " + total);
+    }
+}
