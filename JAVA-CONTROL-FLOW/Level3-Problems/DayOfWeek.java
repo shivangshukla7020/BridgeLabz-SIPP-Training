a5ab99af6366ad1e4da04e82ffe0575b1ad4bbb8
shivangshukla7020 <shivangshukla201@gmail.com>
2025-08-14 00:00:00 +0530

    [shivang] Add : JAVA-CONTROL-FLOW/Level3-Problems/DayOfWeek.java

---
+++ JAVA-CONTROL-FLOW/Level3-Problems/DayOfWeek.java
@@ -0,0 +1,12 @@
+public class DayOfWeek {
+    public static void main(String[] args) {
+        int m = Integer.parseInt(args[0]);
+        int d = Integer.parseInt(args[1]);
+        int y = Integer.parseInt(args[2]);
+        int yo = y - (14 - m) / 12;
+        int x = yo + yo / 4 - yo / 100 + yo / 400;
+        int mo = m + 12 * ((14 - m) / 12) - 2;
+        int dow = (d + x + 31 * mo / 12) % 7;
+        System.out.println(dow);
+    }
+}
