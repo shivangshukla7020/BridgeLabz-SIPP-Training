535208461123c6af70c6a3f80c186c920332c11a
shivangshukla7020 <shivangshukla201@gmail.com>
2025-08-18 00:00:00 +0530

    [shivang] Add : JAVA-CONTROL-FLOW/Level1-Problems/CountdownWhile.java

---
+++ JAVA-CONTROL-FLOW/Level1-Problems/CountdownWhile.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class CountdownWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        while (n >= 1) {
+            System.out.println(n);
+            n--;
+        }
+    }
+}
