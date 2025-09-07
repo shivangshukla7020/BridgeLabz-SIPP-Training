ba06354d748ab9c552392100b8a6e90544751c04
shivangshukla7020 <shivangshukla201@gmail.com>
2025-09-08 00:00:00 +0530

    [shivang] Add : JAVA-CONTROL-FLOW/Level2-Problems/MultiplesBelow100For.java

---
+++ JAVA-CONTROL-FLOW/Level2-Problems/MultiplesBelow100For.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class MultiplesBelow100For {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n > 0 && n < 100) {
+            for (int i = 100; i >= 1; i--) {
+                if (i % n == 0) {
+                    System.out.println(i);
+                }
+            }
+        } else {
+            System.out.println("Invalid input.");
+        }
+    }
+}
