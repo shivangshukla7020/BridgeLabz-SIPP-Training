6ab5e378b5cadccc752f0abd531fc79b91a02e35
shivangshukla7020 <shivangshukla201@gmail.com>
2025-09-21 00:00:00 +0530

    [shivang] Add : JAVA-INHERITANCE/SINGLE-INHERITANCE/Book.java

---
+++ JAVA-INHERITANCE/SINGLE-INHERITANCE/Book.java
@@ -0,0 +1,32 @@
+class Book {
+    String title;
+    int publicationYear;
+
+    Book(String title, int year) {
+        this.title = title;
+        this.publicationYear = year;
+    }
+}
+
+class Author extends Book {
+    String name;
+    String bio;
+
+    Author(String title, int year, String name, String bio) {
+        super(title, year);
+        this.name = name;
+        this.bio = bio;
+    }
+
+    void displayInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Publication Year: " + publicationYear);
+        System.out.println("Author: " + name);
+        System.out.println("Bio: " + bio);
+    }
+
+    public static void main(String[] args) {
+        Author author = new Author("The Java Handbook", 2021, "James Gosling", "Creator of Java");
+        author.displayInfo();
+    }
+}
