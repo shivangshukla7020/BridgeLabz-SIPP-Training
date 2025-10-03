4f72e0c44ede9262be6c1e81ddd18d4e1fe04611
shivangshukla7020 <shivangshukla201@gmail.com>
2025-10-04 00:00:00 +0530

    [shivang] Add : JAVA-METHODS/ChocolateDivider.java

---
+++ JAVA-METHODS/ChocolateDivider.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class ChocolateDivider {
+    public static int[] findRemainderAndQuotient(int number, int divisor) {
+        return new int[]{number / divisor, number % divisor};
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int chocolates = sc.nextInt();
+        int children = sc.nextInt();
+        int[] result = findRemainderAndQuotient(chocolates, children);
+        System.out.println(result[0]);
+        System.out.println(result[1]);
+    }
+}
