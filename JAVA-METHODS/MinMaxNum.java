90867b39c61a9f2e23e4f88c100ecaae6d2af27f
shivangshukla7020 <shivangshukla201@gmail.com>
2025-10-22 00:00:00 +0530

    [shivang] Add : JAVA-METHODS/MinMaxNum.java

---
+++ JAVA-METHODS/MinMaxNum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class MinMaxNum {
+    public static int[] findSmallestAndLargest(int a, int b, int c) {
+        int min = Math.min(a, Math.min(b, c));
+        int max = Math.max(a, Math.max(b, c));
+        return new int[]{min, max};
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+        int[] res = findSmallestAndLargest(a, b, c);
+        System.out.println(res[0]);
+        System.out.println(res[1]);
+    }
+}
