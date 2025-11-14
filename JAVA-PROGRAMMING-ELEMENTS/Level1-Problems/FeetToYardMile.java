b18766fd01c91283bb3e032233ba6fb8e13776f2
shivangshukla7020 <shivangshukla201@gmail.com>
2025-11-15 00:00:00 +0530

    [shivang] Add : JAVA-PROGRAMMING-ELEMENTS/Level1-Problems/FeetToYardMile.java

---
+++ JAVA-PROGRAMMING-ELEMENTS/Level1-Problems/FeetToYardMile.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class FeetToYardMile{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double distanceInFeet = sc.nextDouble();
+
+        double distanceInYards = distanceInFeet / 3;
+        double distanceInMiles = distanceInYards / 1760;
+
+
+        System.out.println("Distance is " + distanceInYards + " yards and " + distanceInMiles + " miles while in feet is " + distanceInFeet);
+    }
+}
