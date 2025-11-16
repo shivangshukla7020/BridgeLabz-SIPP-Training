2d2e4263278d21d4acc6dc2e2fd75263bcd4d9b6
shivangshukla7020 <shivangshukla201@gmail.com>
2025-11-17 00:00:00 +0530

    [shivang] Add : JAVA-PROGRAMMING-ELEMENTS/Level1-Problems/HarryAge.java

---
+++ JAVA-PROGRAMMING-ELEMENTS/Level1-Problems/HarryAge.java
@@ -0,0 +1,8 @@
+public class HarryAge{
+    public static void main(String[] args){
+        int curryear = 2024;
+        int birthYear = 2000;
+        int age = curryear - birthYear;
+        System.out.println("Harry's age in 2024 is " + age);
+    }
+}
