5e492d0294927d877839f5b9d90143cc395d58d5
shivangshukla7020 <shivangshukla201@gmail.com>
2025-11-29 00:00:00 +0530

    [shivang] Add : JAVA-PROGRAMMING-ELEMENTS/Level2-Problems/TotalPurchase.java

---
+++ JAVA-PROGRAMMING-ELEMENTS/Level2-Problems/TotalPurchase.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class TotalPurchase {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int totalStudents = sc.nextInt();
+        int totalhandshakes = (totalStudents * (totalStudents - 1)) / 2;
+
+        System.out.println("The total number of possible handshakes is " + totalhandshakes);
+    }
+}
