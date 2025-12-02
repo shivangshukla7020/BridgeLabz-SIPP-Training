7896b61c5f7318fd5686e981a9a5bc632f7e19fe
shivangshukla7020 <shivangshukla201@gmail.com>
2025-12-03 00:00:00 +0530

    [shivang] Add : JAVA-STRING-HANDLING/countVowelsConsonants.java

---
+++ JAVA-STRING-HANDLING/countVowelsConsonants.java
@@ -0,0 +1,17 @@
+
+public class countVowelsConsonants {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        int vowels = 0, consonants = 0;
+        str = str.toLowerCase();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+                if ("aeiou".indexOf(ch) != -1) vowels++;
+                else consonants++;
+            }
+        }
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+    }
+}
