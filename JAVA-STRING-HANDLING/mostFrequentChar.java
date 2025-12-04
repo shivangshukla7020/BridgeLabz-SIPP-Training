3d49906e9ead6b2d6c9bacaebec28490f8e01332
shivangshukla7020 <shivangshukla201@gmail.com>
2025-12-05 00:00:00 +0530

    [shivang] Add : JAVA-STRING-HANDLING/mostFrequentChar.java

---
+++ JAVA-STRING-HANDLING/mostFrequentChar.java
@@ -0,0 +1,19 @@
+
+public class mostFrequentChar {
+    public static void main(String[] args) {
+        String str = "success";
+        int[] freq = new int[256];
+        for (int i = 0; i < str.length(); i++) {
+            freq[str.charAt(i)]++;
+        }
+        int max = 0;
+        char result = ' ';
+        for (int i = 0; i < str.length(); i++) {
+            if (freq[str.charAt(i)] > max) {
+                max = freq[str.charAt(i)];
+                result = str.charAt(i);
+            }
+        }
+        System.out.println("Most Frequent Character: " + result);
+    }
+}
