328bd86d4c475105b2adef1080437aa26f244041
shivangshukla7020 <shivangshukla201@gmail.com>
2025-12-06 00:00:00 +0530

    [shivang] Add : JAVA-STRING-HANDLING/palindromeCheck.java

---
+++ JAVA-STRING-HANDLING/palindromeCheck.java
@@ -0,0 +1,12 @@
+
+public class palindromeCheck {
+    public static void main(String[] args) {
+        String str = "madam";
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+        if (str.equals(reversed)) System.out.println("Palindrome");
+        else System.out.println("Not a Palindrome");
+    }
+}
