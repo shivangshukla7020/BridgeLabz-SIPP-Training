081c09cf7a98e2b9b7f2edc1c42a6e0bdea9f99f
shivangshukla7020 <shivangshukla201@gmail.com>
2025-12-11 00:00:00 +0530

    [shivang] Add : JAVA-STRING-HANDLING/substringOccurrences.java

---
+++ JAVA-STRING-HANDLING/substringOccurrences.java
@@ -0,0 +1,12 @@
+
+public class substringOccurrences {
+    public static void main(String[] args) {
+        String str = "banana";
+        String sub = "ana";
+        int count = 0;
+        for (int i = 0; i <= str.length() - sub.length(); i++) {
+            if (str.substring(i, i + sub.length()).equals(sub)) count++;
+        }
+        System.out.println("Occurrences: " + count);
+    }
+}
