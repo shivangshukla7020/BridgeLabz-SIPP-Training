341315d7ede35dda543efa4d89d35b14f4108ecc
shivangshukla7020 <shivangshukla201@gmail.com>
2025-12-16 00:00:00 +0530

    [shivang] Add : JAVA-STRING-HANDLING/toggleCase.java

---
+++ JAVA-STRING-HANDLING/toggleCase.java
@@ -0,0 +1,14 @@
+
+public class toggleCase {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (Character.isUpperCase(ch)) result += Character.toLowerCase(ch);
+            else if (Character.isLowerCase(ch)) result += Character.toUpperCase(ch);
+            else result += ch;
+        }
+        System.out.println("Toggled Case: " + result);
+    }
+}
