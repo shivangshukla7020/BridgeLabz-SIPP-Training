ab00a04d7d3af49f21d0a7708bb1e90e574992ed
shivangshukla7020 <shivangshukla201@gmail.com>
2025-12-19 00:00:00 +0530

    [shivang] Add : JAVA-STRINGS/CharTypeDisplay.java

---
+++ JAVA-STRINGS/CharTypeDisplay.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class CharTypeDisplay {
+    public static String getType(char ch) {
+        if (ch >= 'A' && ch <= 'Z') ch = (char)(ch + 32);
+        if (ch >= 'a' && ch <= 'z') {
+            if ("aeiou".indexOf(ch) != -1) return "Vowel";
+            else return "Consonant";
+        }
+        return "Not a Letter";
+    }
+
+    public static String[][] analyzeText(String text) {
+        String[][] result = new String[text.length()][2];
+        for (int i = 0; i < text.length(); i++) {
+            result[i][0] = String.valueOf(text.charAt(i));
+            result[i][1] = getType(text.charAt(i));
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+        String[][] result = analyzeText(text);
+        for (String[] row : result) {
+            System.out.println(row[0] + "\t" + row[1]);
+        }
+    }
+}
